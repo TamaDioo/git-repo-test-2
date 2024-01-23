e05d89b0bb23ec42f1f80892977740f0f838e779
Tama Dioo <tamadio91@gmail.com>
2024-01-23 14:23:11 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hallo Dunia!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore repudiandae saepe debitis culpa molestiae eum unde sequi? Quisquam aperiam in vitae, repellat ullam blanditiis ratione voluptatum dignissimos deleniti quam corporis?</p>
 </body>
 </html>
